a3eba4399cdd4d4c9a69995cdcfed4ea7ad68bb5
Justin Faust <jfaust@onestopsystems.com>
2018-11-26 21:23:29 -0700

    a real design we can be proud of

--- unbear/src/assets/js/app.js
+++ unbear/src/assets/js/app.js
@@ -1,12 +1,15 @@
-import $ from 'jquery';
-// noinspection ES6UnusedImports
-import whatInput from 'what-input';
+import $ from 'jquery'
+import whatInput from 'what-input'
 
-// noinspection JSUnresolvedVariable
-window.$ = $;
+window.$ = $
 
-// noinspection ES6UnusedImports
-import Foundation from 'foundation-sites';
+import Foundation from 'foundation-sites'
+
+if (Foundation) {
+  // webpack will exclude foundation from
+  // the build unless you write an expression
+  // using `Foundation`
+}
 
 // If you want to pick and choose which
 // modules to include, comment out the
@@ -15,4 +18,13 @@ import Foundation from 'foundation-sites';
 // import './lib/foundation-explicit-pieces';
 
 // boot up foundation
-$(document).foundation();
+$(document).foundation()
+
+const FontFaceObserver = require('font-face-observer')
+new FontFaceObserver('Source Sans Pro Bold')
+    .check()
+    .then(() => {
+      $('body').addClass('font-loaded')
+    }, () => {
+      /* failed */
+    })
